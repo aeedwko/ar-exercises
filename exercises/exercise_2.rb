44f0cb5d55201a1ecfdbb12302f9305ce00a8699
Edwin Ko <edwin.mk.ko@gmail.com>
2024-04-16 20:16:52 +0000

    complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.find(1) # same as Store.find_by(id: 1)
+@store2 = Store.find(2)
+
+puts "First store name before update: #{@store1.name}"
+
+@store1.name = "Whistler"
+@store1.save
+
+puts "First store name after update: #{@store1.name}"
